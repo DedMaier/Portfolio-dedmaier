3fbbb5d57b8745aab3e431ed2a14a7668f43e83f
AppVeyor <ci@appveyor.com>
2023-11-28 00:57:26 +0300

    kommit

--- mail.php
+++ mail.php
@@ -5,23 +5,32 @@ require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
 $title = "Тема письма";
-$file = $_FILES['file'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$text = $_POST['text'];
+// $file = $_FILES['file'];
 
 $c = true;
 // Формирование самого письма
 $title = "Заголовок письма";
-foreach ( $_POST as $key => $value ) {
-  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-    $body .= "
-    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-    </tr>
-    ";
-  }
-}
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$text
+";
+// foreach ( $_POST as $key => $value ) {
+//   if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+//     $body .= "
+//     " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+//       <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+//       <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+//     </tr>
+//     ";
+//   }
+// }
 
-$body = "<table style='width: 100%;'>$body</table>";
+// $body = "<table style='width: 100%;'>$body</table>";
 
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -38,25 +47,25 @@ try {
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
 
-  $mail->setFrom('slava071172@mail.ru', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
+  $mail->setFrom('slava071172@mail.ru', 'Вячеслав'); // Адрес самой почты и имя отправителя
 
   // Получатель письма
   $mail->addAddress('slava071172@mail.ru');
   $mail->addAddress('slava19721107@mail.ru');
 
   // Прикрипление файлов к письму
-  if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-      $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-      $filename = $file['name'][$ct];
-      if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-          $mail->addAttachment($uploadfile, $filename);
-          $rfile[] = "Файл $filename прикреплён";
-      } else {
-          $rfile[] = "Не удалось прикрепить файл $filename";
-      }
-    }
-  }
+  // if (!empty($file['name'][0])) {
+  //   for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+  //     $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+  //     $filename = $file['name'][$ct];
+  //     if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+  //         $mail->addAttachment($uploadfile, $filename);
+  //         $rfile[] = "Файл $filename прикреплён";
+  //     } else {
+  //         $rfile[] = "Не удалось прикрепить файл $filename";
+  //     }
+  //   }
+  // }
 
   // Отправка сообщения
   $mail->isHTML(true);
