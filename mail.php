5e4f2a2e16b48a7af0c3cc774e3d5e5c366a9b7d
AppVeyor <ci@appveyor.com>
2023-11-27 20:21:53 +0300

    kommit

--- mail.php
+++ mail.php
@@ -34,7 +34,7 @@ try {
   // Настройки вашей почты
   $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
   $mail->Username   = 'slava071172@mail.ru'; // Логин на почте
-  $mail->Password   = 'vjUGyBUemEiZC5rMJXBk'; // Пароль на почте
+  $mail->Password   = 'jMuB0fEdjqiunQgy7J2U'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
 
