ee42f810ae90358ab9eb5f274ad6a68479e7c1a2
AppVeyor <ci@appveyor.com>
2023-11-29 10:03:44 +0300

    kommit

--- mail.php
+++ mail.php
@@ -4,33 +4,29 @@ require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-$title = "Тема письма";
+// Переменные, которые отправляет пользователь
 $name = $_POST['name'];
-$phone = $_POST['phone'];
+$tel = $_POST['tel'];
 $text = $_POST['text'];
+
+// $title = "Тема письма";
 // $file = $_FILES['file'];
 
 $c = true;
-// Формирование самого письма
-$title = "Заголовок письма";
-$body = "
-<h2>Новое письмо</h2>
-<b>Имя:</b> $name<br>
-<b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$text
-";
-// foreach ( $_POST as $key => $value ) {
-//   if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-//     $body .= "
-//     " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-//       <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-//       <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-//     </tr>
-//     ";
-//   }
-// }
+//Формирование самого письма
+$title = "Письмо с сайта Портфолио";
+foreach ( $_POST as $key => $value ) {
+  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+    $body .= "
+    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+    </tr>
+    ";
+  }
+}
 
-// $body = "<table style='width: 100%;'>$body</table>";
+$body = "<table style='width: 100%;'>$body</table>";
 
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -39,14 +35,11 @@ try {
   $mail->isSMTP();
   $mail->CharSet = "UTF-8";
   $mail->SMTPAuth   = true;
-  $mail->SMTPDebug = 2;
-  $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
-
 
   // Настройки вашей почты
   $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
-  $mail->Username   = 'slava071172@mail.ru'; // Логин на почте
-  $mail->Password   = 'vjUGyBUemEiZC5rMJXBk'; // Пароль на почте
+  $mail->Username   = 'slava071172'; // Логин на почте
+  $mail->Password   = 'kkje gfdu yxmq whuk'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
 
@@ -56,7 +49,7 @@ try {
   $mail->addAddress('slava071172@mail.ru');
   $mail->addAddress('slava19721107@mail.ru');
 
-  // Прикрипление файлов к письму
+  //Прикрипление файлов к письму
   // if (!empty($file['name'][0])) {
   //   for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
   //     $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
@@ -75,21 +68,8 @@ try {
   $mail->Subject = $title;
   $mail->Body = $body;
 
-//   $mail->send();
-
-// } catch (Exception $e) {
-//   $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
-// }
-
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";} 
-else {$result = "error";}
+  $mail->send();
 
 } catch (Exception $e) {
-    $result = "error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
-
-// Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
-
